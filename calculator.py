0770288ebf59e341032d58234898c43d287cab1f
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-02 18:40:19 +0100

    calculator solution

--- calculator.py
+++ calculator.py
@@ -1,12 +1,23 @@
-def add(a, b)
+def add(a, b):
     return a + b
-def sub(a, b)
+def sub(a, b):
     return a - b
-def mul(a, b)
+def mul(a, b):
     return a * b
-def div(a, b)
+def div(a, b):
     return a / b
 
+operations = {
+    "+" : add,
+    "-" : sub,
+    "*" : mul,
+    "/" : div
+}
 
 def start():
-    pass
+    num1 = int(input("First number:\n"))
+    num2 = int(input("Second number:\n"))
+    for symbol in operations:
+        print(symbol)
+    operation_symbol = input("Operation: ")
+    print(f"{num1} {operation_symbol} {num2} = {operations[operation_symbol](num1, num2)}")

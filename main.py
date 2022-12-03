2b62ec45de6a41746e6d393facda1d567b5bae67
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-03 20:28:23 +0100

    days in month project init

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import calculator
 import blackjack
 import number_guess
+import days_in_month
 
 # calculator.start()
-
 # blackjack.start()
-number_guess.start()
+# number_guess.start()
+days_in_month.start()
 

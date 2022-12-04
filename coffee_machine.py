11e87c1efa34940dc98f3d9109ebb056c1f25283
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-04 19:58:03 +0100

    coffee machine project init

---
+++ coffee_machine.py
@@ -0,0 +1,12 @@
+import coffee_machine_data
+
+milk = 300
+
+def start():
+    show_stats()
+
+
+def show_stats():
+    print(f"Milk: {milk}")
+
+
--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import blackjack
 import number_guess
 import days_in_month
 import high_low_game
+import coffee_machine
 
 # calculator.start()
 # blackjack.start()
 # number_guess.start()
 # days_in_month.start()
-high_low_game.start()
+# high_low_game.start()
+coffee_machine.start()
 

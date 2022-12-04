89727e280e864d3f3f2023c929ad3a18f318b2cd
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-04 16:21:36 +0100

    high low v1.0

--- high_low_game.py
+++ high_low_game.py
@@ -1,10 +1,30 @@
+import random
+
 from high_low_data import data
 
 def start():
-    guess = input(f"{data[0]['name']} (a) VS {data[1]['name']} (b)\n")
-    if guess == "a":
-        print("a")
-    elif guess == "b":
-        print("b")
-    else:
-        print("wrong input")
+    game_running = True
+    score = 0
+    while game_running:
+        quest01 = random.choice(data)
+        while True:
+            quest02 = random.choice(data)
+            if quest01['name'] != quest02['name']:
+                break
+
+        print(f"\nScore = {score}")
+        print("Who has more follower?")
+        guess = input(f"{quest01['name']} (a) VS {quest02['name']} (b) - end game (c)\n")
+
+        if guess == "a" and quest01['follower_count'] > quest02['follower_count']:
+            print("You are right")
+            score += 1
+        elif guess == "b"  and quest02['follower_count'] > quest01['follower_count']:
+            print("You are right")
+            score += 1
+        elif guess == "c":
+            game_running = False
+        else:
+            print("That is wrong")
+        print(f"{quest01['name']} = {quest01['follower_count']}")
+        print(f"{quest02['name']} = {quest02['follower_count']}")

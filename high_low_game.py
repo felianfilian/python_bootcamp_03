7ea20d4384b173015d627ea902a18a8c63b2325d
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-04 19:52:50 +0100

    code optimization

--- high_low_game.py
+++ high_low_game.py
@@ -12,19 +12,24 @@ def start():
             if quest01['name'] != quest02['name']:
                 break
 
+        a_name = quest01['name']
+        b_name = quest02['name']
+        a_follower = quest01['follower_count']
+        b_follower = quest02['follower_count']
+
         print(f"\nScore = {score}")
         print("Who has more follower?")
-        guess = input(f"{quest01['name']} (a) VS {quest02['name']} (b) - end game (c)\n")
+        guess = input(f"{a_name} (a) VS {b_name} (b) - end game (c)\n").lower()
 
-        if guess == "a" and quest01['follower_count'] > quest02['follower_count']:
+        if guess == "a" and a_follower > b_follower:
             print("You are right")
             score += 1
-        elif guess == "b"  and quest02['follower_count'] > quest01['follower_count']:
+        elif guess == "b"  and b_follower > a_follower:
             print("You are right")
             score += 1
         elif guess == "c":
             game_running = False
         else:
             print("That is wrong")
-        print(f"{quest01['name']} = {quest01['follower_count']}")
-        print(f"{quest02['name']} = {quest02['follower_count']}")
+        print(f"{a_name} = {a_follower}")
+        print(f"{b_name} = {b_follower}")

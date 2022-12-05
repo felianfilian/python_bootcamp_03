10233a9c3b3e5ba99eb78e9d8da228382725bef2
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-12-05 10:08:52 +0100

    class tests 2

--- class_tests.py
+++ class_tests.py
@@ -2,4 +2,7 @@ from prettytable import PrettyTable
 
 def start():
     table = PrettyTable()
+    table.add_column("Pokemon Name", ["Pikachu", "Bisasam", "Glurak"])
+    table.add_column("Pokemon Type", ["electric", "flower", "fire"])
+    table.align = "l"
     print(table)
